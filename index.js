593d800fb18e52cc908cd26b0bd9fdc67b996a8f
Digita1Panda <prodigykyo@gmail.com>
2024-02-19 03:14:35 +0000

    added more questions

--- index.js
+++ index.js
@@ -17,6 +17,32 @@ const questions = [
   {
     type: "input",
     name: "installation",
-    message: "Please provide any installation instructions for your project",
+    message: "Please provide any installation instructions for your project.",
+  },
+  // Usage information
+  {
+    type: "input",
+    name: "usage",
+    message: "What is the usage for this project/application?",
+  },
+  //   License selection
+  {
+    type: "checkbox",
+    name: "license",
+    message: [
+      "MIT",
+      "Apache License 2.0",
+      "GNU General Public License (GPL) v3",
+      "Mozilla Public License 2.0",
+      "BSD 3-Clause License",
+      "N/A",
+    ],
+  },
+  //   People who contributed
+  {
+    type: "input",
+    name: "contributors",
+    message:
+      "Please list any contributors to the project or state how people can contribute to the project.",
   },
 ];
