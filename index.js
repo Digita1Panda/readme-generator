f3fcb2689fb99b7fd4bd5f9cd4615946fd9fef27
Digita1Panda <prodigykyo@gmail.com>
2024-02-19 02:51:54 +0000

    added some questions

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const inquirer = require("inquirer");
 
-const questions = [];
+const questions = [
+  // Title of the project
+  {
+    type: "input",
+    name: "title",
+    message: "What is the project title?",
+  },
+  // Description section
+  {
+    type: "input",
+    name: "description",
+    message: "Please enter a description for your project.",
+  },
+  // Installation instructions
+  {
+    type: "input",
+    name: "installation",
+    message: "Please provide any installation instructions for your project",
+  },
+];
